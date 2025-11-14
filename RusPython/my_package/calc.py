2384fe18d2cb773707ef1eb797b2b541f340d2aa
Andrey <demovas@yandex.ru>
2025-11-14 08:53:39 +0300

    module test_calculator_pytest_added

--- RusPython/my_package/calc.py
+++ RusPython/my_package/calc.py
@@ -1,29 +1,21 @@
-def calculator(expression: str) -> float:
+def calculator(expression):
     allowed = '+-/*'
     if not any(sign in expression for sign in allowed):
         raise ValueError(f'Выражение должно содержать хотя бы один из знаков {allowed}!')
-
     for sign in allowed:
         if sign in expression:
             try:
-                left, right = [int(item.strip()) for item in expression.split(sign)]
-                if sign == '+':
-                    return left + right
-                elif sign == '-':
-                    return left - right
-                elif sign == '/':
-                    return left / right
-                else:
-                    return left * right
+                left, right = expression.split(sign)
+                left, right = int(left), int(right)
+                return {
+                    '+': lambda a, b: a + b,
+                    '-': lambda a, b: a - b,
+                    '*': lambda a, b: a * b,
+                    '/': lambda a, b: a / b,
+                }[sign](left, right)
             except (ValueError, TypeError):
                 raise ValueError('Выражение должно содержать 2 целых числа и 1 знак!')
 
 
 if __name__ == '__main__':
-    print(calculator('ф * 2'))
-
-
-
-
-
-
+    print(calculator('2 * 2'))
