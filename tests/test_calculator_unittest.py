c68994b54ee5be7c572e17cc40f1975290692376
Andrey <demovas@yandex.ru>
2025-11-18 08:43:04 +0300

    four unittests added

---
+++ tests/test_calculator_unittest.py
@@ -0,0 +1,20 @@
+from unittest import TestCase, main
+from RusPython.my_package.calc import calculator as calc
+
+
+class Calculator(TestCase):
+    def test_plus(self):
+        self.assertEqual(calc('2+2'), 4)
+
+    def test_minus(self):
+        self.assertEqual(calc('2-2'), 0)
+
+    def test_div(self):
+        self.assertEqual(calc('3/3'), 1)
+
+    def test_mul(self):
+        self.assertEqual(calc('2*4'), 8)
+
+
+if __name__ == '__main__':
+    main()
